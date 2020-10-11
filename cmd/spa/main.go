d0f0dd0c401aaa94b04e828c702cbf7b08947a4b
MTReload <suhonosik.dmitry@gmail.com>
2020-10-11 11:51:27 +0300

    Upd

--- cmd/spa/main.go
+++ cmd/spa/main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/mtreload/zh/pkg/config"
 )
 
 func main() {
+	configPath := flag.String("c", "./config.yml", "path to config")
+	flag.Parse()
 
-	fs := http.FileServer(http.Dir("spa"))
-	log.Fatal(http.ListenAndServe(":80", fs))
+	log := logrus.New()
+
+	cfg := &config.Config{}
+
+	err := config.Read(*configPath, cfg)
+	if err != nil {
+		log.WithError(err).Fatal("can't read config")
+	}
+
+	index, err := ioutil.ReadFile(cfg.Index)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Fatal(http.ListenAndServe(cfg.FrontAddr, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Header().Add("Content-type", "text/html")
+
+		w.Write(index)
+	})))
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,4 +3,6 @@ package config
 type Config struct {
 	Database   string `yaml:"database"`
 	ListenAddr string `yaml:"listen_addr"`
+	FrontAddr  string `yaml:"front_addr"`
+	Index      string `yaml:"index"`
 }
